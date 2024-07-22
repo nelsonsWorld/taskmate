6180c03cce22225c817195afcbf84dd3e3045e9f
Nelson Zuluaga <nelson.b.zuluaga@gmail.com>
2024-07-22 09:28:24 -0400

    Fixed html issues for app/views.py

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def todolist(request):
-    return HttpResponse("Welcome To Task Page")
+    return render(request,'todolist.html', {})
+    #return HttpResponse("Welcome To Task Page")
