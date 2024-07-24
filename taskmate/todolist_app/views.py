ceededb1582c7252b3d5f6a1accdbec51b57244c
Nelson Zuluaga <nelson.b.zuluaga@gmail.com>
2024-07-23 20:58:12 -0400

    We're cooking now!

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 def todolist(request):
     return render(request,'todolist.html', {})
     #return HttpResponse("Welcome To Task Page")
+
+
+def contact(request):
+    return render(request,'contact.html', {})
+
+def about(request):
+    return render(request, 'about.html', {})
