8c4487e6efe14c4430d1fbf0295200e64cc7d88a
Nelson Zuluaga <nelson.b.zuluaga@gmail.com>
2024-07-24 23:28:21 -0400

    Changes, added jinja code to todolist_app

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def todolist(request):
-    return render(request,'todolist.html', {})
+    return render(request,'todolist.html', {"welcome_text":"Yeahh!!!!"})
     #return HttpResponse("Welcome To Task Page")
 
 
 def contact(request):
-    return render(request,'contact.html', {})
+    context = {
+        'welcome_text':"Welcome Contact Page.",
+    }
+    return render(request,'contact.html', context)
 
 def about(request):
-    return render(request, 'about.html', {})
+    context = {
+        'welcome_text':"Welcome to About Page.",
+    }
+    return render(request, 'about.html', context)
