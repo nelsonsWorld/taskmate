8e49f23401b0976612f18ff87859a78a8fdb6f2d
Nelson Zuluaga <nelson.b.zuluaga@gmail.com>
2024-08-13 19:31:35 -0400

    login page

--- taskmate/users_app/urls.py
+++ taskmate/users_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from users_app import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register', views.register, name = 'register'),
-
+   # You can create a folder "registration" if you want to use 
+   # the default directory for your login/logout templates
+    path('login', auth_views.LoginView.as_view(template_name='login.html'), name= 'login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout')
 ]
