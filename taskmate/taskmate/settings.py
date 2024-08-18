c6d2ef1dcd790f51bf5f276d4e37487dfd0d7b8e
Nelson Zuluaga <nelson.b.zuluaga@gmail.com>
2024-08-17 23:06:16 -0400

    This version depends on PostGres

--- taskmate/taskmate/settings.py
+++ taskmate/taskmate/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ru)m*wi(4sm6v45_k2@-%k=m+g&d3ua6lwa1s_@okim8(4-he^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -78,12 +78,23 @@ WSGI_APPLICATION = 'taskmate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': BASE_DIR / 'db.sqlite3',
+#      }
+#  }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+       'default': {
+          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+          'NAME': 'taskmate01',
+          'USER': 'postgres',
+          'PASSWORD': 'taskmate01',
+          'HOST': '127.0.0.1',
+          'PORT':'5432',
+      }
+  }
 
 
 # Password validation
